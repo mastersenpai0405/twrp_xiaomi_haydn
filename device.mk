476e1af49543fa4ae44f4490eb2870e41be7937b
mastersenpai05 <pampangavergil0405@gmail.com>
2021-08-24 00:51:20 +0800

    haydn: add to fix decryption && touch

--- device.mk
+++ device.mk
@@ -59,6 +59,11 @@ PRODUCT_PACKAGES += \
     update_engine \
     update_verifier \
     update_engine_sideload
+
+# qcom decryption
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
     
 # Fastbootd
 PRODUCT_PACKAGES += \
